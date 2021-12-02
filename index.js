c0318e194fa0ceb67a503e5f0388262051d96213
Nima sherpa <Toplouie78@gmail.com>
2021-12-02 15:54:54 -0500

    first push of the branch

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
-      throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
-  }
-  
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
-  
+	if (!c.every((a) => Number.isNumber(a)))
+		throw "All arguments must be numbers.";
+	return c.reduce((a, b) => a + b);
+}
+
+fn(1, 2, 3); //> 6
+fn(10, "B", 20); //> error All arguments must be numbers.
+
+// Nima branch
+
+function everyFunction(...nums) {
+	if (!nums.every((each) => Number.isNumber(each)))
+		throw "All argument must be numbers.";
+	return nums.reduce((a, b) => a + b);
+}
