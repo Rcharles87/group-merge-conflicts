bebfb8b668fcf2f89adf17fe73c825271fff6969
hinkabi <hinkabi@gmail.com>
2021-12-02 16:01:21 -0500

    changes to line 8

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function fn(...c) {
   }
   
   fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  fn(10, "Z", 20); //> error All arguments in this function must be a number.
   
