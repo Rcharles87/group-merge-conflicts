3d32ede87f77b00fa273a813357d051e79dbbd60
Nima sherpa <Toplouie78@gmail.com>
2021-12-02 16:11:45 -0500

    added extra parameters

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 function fn(...c) {
 	if (!c.every((a) => Number.isNumber(a)))
 		throw "All arguments must be numbers.";
-	return c.reduce((a, b) => a + b);
+	return c.reduce((a, b, c, d) => a + b);
 }
 
 fn(1, 2, 3); //> 6
 fn(10, "B", 20); //> error All arguments must be numbers.
-
-// Nima branch
-
-function everyFunction(...nums) {
-	if (!nums.every((each) => Number.isNumber(each)))
-		throw "All argument must be numbers.";
-	return nums.reduce((a, b) => a + b);
-}
